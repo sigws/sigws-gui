974870eb711e801b5ff2927098d5ec52c92f65a9
SkyTeamWater <skyteamwater@qq.com>
2021-03-21 09:51:29 +0800

    update dll.go

---
+++ simple-good-web-server/dll.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"C"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+var FlagToCallPause bool
+
+//export StartListenAndServe
+func StartListenAndServe(port int) {
+	FlagToCallPause = false
+	PORT := strconv.Itoa(port)
+	http.Handle("/", http.FileServer(http.Dir("www")))
+	err := http.ListenAndServe(":"+PORT, nil)
+	http.ListenAndServe(":"+PORT, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func main() {
+	// Need a main function to make CGO compile package as C shared library
+}
