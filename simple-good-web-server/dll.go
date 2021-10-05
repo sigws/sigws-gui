467573c4a1345278513c085199a6ad40771b0a61
skyteamwater@qq.com <skyteamwater@qq.com>
2021-10-05 10:06:58 +0800

    update

--- simple-good-web-server/dll.go
+++ simple-good-web-server/dll.go
@@ -2,19 +2,74 @@ package main
 
 import (
 	"C"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 )
+
+var StartTime string
+
+func nowtime() string {
+	now := time.Now()
+	year := strconv.Itoa(now.Year())
+	month := strconv.Itoa(int(now.Month()))
+	day := strconv.Itoa(now.Day())
+	hour := strconv.Itoa(now.Hour())
+	minute := strconv.Itoa(now.Minute())
+	second := strconv.Itoa(now.Second())
+	return "[" + year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + second + "] "
+}
+
+func starttime() string {
+	now := time.Now()
+	year := strconv.Itoa(now.Year())
+	month := strconv.Itoa(int(now.Month()))
+	day := strconv.Itoa(now.Day())
+	hour := strconv.Itoa(now.Hour())
+	minute := strconv.Itoa(now.Minute())
+	second := strconv.Itoa(now.Second())
+	return "sig-logs/[" + year + "-" + month + "-" + day + " " + hour + "-" + minute + "-" + second + "].log"
+}
+
+func printlog(content string) {
+	f, err := os.OpenFile(StartTime, os.O_RDONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Println("open file error:", err)
+		return
+	}
+	_, err = f.WriteString(nowtime() + content + "\n")
+	if err != nil {
+		log.Println("close file error:", err)
+		return
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Println(nowtime(), err)
+		}
+	}(f)
+	fmt.Println(nowtime(), content)
+}
 
 var FlagToCallPause bool
 
 //export StartListenAndServe
 func StartListenAndServe(port int) {
+	StartTime = starttime()
+	err := os.MkdirAll("sig-logs", os.ModePerm)
+	if err != nil {
+		fmt.Println(nowtime(), err)
+	} else {
+		printlog("Create logs folder successfully!")
+	}
 	FlagToCallPause = false
 	PORT := strconv.Itoa(port)
+	printlog("Start listen on " + PORT)
 	http.Handle("/", http.FileServer(http.Dir("www")))
-	err := http.ListenAndServe(":"+PORT, nil)
+	err = http.ListenAndServe(":"+PORT, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
